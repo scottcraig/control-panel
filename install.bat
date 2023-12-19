8b621816c8615096f4e99aefe2ccb9d95a0f8419
matt <matt@dont-ping.me>
2023-12-19 13:46:45 -0800

    remove PATH

--- install.bat
+++ install.bat
@@ -3,15 +3,15 @@
 @REM this temporary until git is installed in the lab, also dependent on version number (app-3.3.6)
 SET git="C:\Users\%USERNAME%\AppData\Local\GithubDesktop\app-3.3.6\resources\app\git\git.exe"
 
-@REM https://stackoverflow.com/a/8995407
-NET SESSION >nul 2>&1
-IF NOT %errorlevel% EQU 0 (
-    ECHO Please escalate permsission to administrator ^(please run as admin^)
-    PAUSE
+@REM @REM https://stackoverflow.com/a/8995407
+@REM NET SESSION >nul 2>&1
+@REM IF NOT %errorlevel% EQU 0 (
+@REM     ECHO Please escalate permsission to administrator ^(please run as admin^)
+@REM     PAUSE
 
-    @REM exit program and returning error code 1 (program failed to run)
-    exit /b 1
-)
+@REM     @REM exit program and returning error code 1 (program failed to run)
+@REM     exit /b 1
+@REM )
 
 @REM program has administrator privilege, continue.
 
@@ -32,12 +32,12 @@ IF NOT EXIST %BINDIR% mkdir %BINDIR%
 cd %BINDIR%
 %git% clone https://github.com/timberline-secondary/control-panel
 
-@REM per https://stackoverflow.com/a/41809116, move batch file to System32
-@REM also, writing to System32 requires administrator override
-copy %BINDIR%\control-panel\control-panel.bat C:\Windows\System32\control-panel.bat
+@REM @REM per https://stackoverflow.com/a/41809116, move batch file to System32
+@REM @REM also, writing to System32 requires administrator override
+@REM copy %BINDIR%\control-panel\control-panel.bat C:\Windows\System32\control-panel.bat
 
-@REM alias of ctrlp for short-hand
-copy %BINDIR%\control-panel\control-panel.bat C:\Windows\System32\ctrlp.bat 
+@REM @REM alias of ctrlp for short-hand
+@REM copy %BINDIR%\control-panel\control-panel.bat C:\Windows\System32\ctrlp.bat 
 
 ECHO ^[^*^] ^- Installed Control Panel, type ^`control-panel^` to begin.
 PAUSE

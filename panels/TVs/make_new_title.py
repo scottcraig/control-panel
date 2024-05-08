f38c47e19c22ea7f8e5d267026fb3ac525729482
scottcraig <scott.craig.victoria@gmail.com>
2024-05-08 09:22:13 -0700

    moddified add new titile to be make new title locally

--- panels/TVs/make_new_title.py
+++ panels/TVs/make_new_title.py
@@ -5,31 +5,25 @@ from .._utils.ssh import SSH
 from ..User_Management import _utils as user_utils
 from .add_new_media import add_new_media, refresh_slideshow
 
-temp_dir = os.environ["TMP"]
-hostname = "hightower"
-SERVER_USERNAME = "pi-slideshow"
 
 
-def add_new_title():
 
-    print("\n ******** \n For this to work, make sure you have Ubuntu fonts installed on the computer, available through Google Fonts: https://fonts.google.com/specimen/Ubuntu\n ********\n")
+def make_new_title():
 
-    fullname, username = user_utils.get_and_confirm_user()
-    if not fullname:
-        return False
+    print("\n ******** \n For this to work, make sure you have Ubuntu fonts installed on the computer, available through Google Fonts: https://fonts.google.com/specimen/Ubuntu \n ********\n")
+    print("\nInkscape also needs to be installed. ********\n")
 
-    fullname = fullname.title()
+    print("\nThe file will be placed in the output folder.\n")
 
-    # gets info of the student who made the art
-    fullname_entered = utils.input_plus("Full name", fullname)
-    if fullname_entered:
-        fullname = fullname_entered
-
-    grad_year = utils.input_styled("Grad Year: \n")
-
-    last_name = username.split(".")[-1].title()  # get the last word if the username tyere.couture will get couture
-
-    # https://pypi.org/project/inquirer/
+    username = utils.input_plus("Enter username for filename (firstname.lastname): ")
+    if username == "q":
+        return
+    fullname = utils.input_plus("Enter the name to appear in the title card: ")
+    if fullname == "q":
+        return
+    grad_year = utils.input_plus("Grad Year: ")
+    if grad_year == "q":
+        return
 
     subject_list = [
         inquirer.List('subject',
@@ -42,22 +36,17 @@ def add_new_title():
 
     # gets user to input a custom subject if they so choose
     if choose_subject == "Custom subject:":
-        custom_subject = utils.input_styled("Well then what are they in? \n")
+        custom_subject = utils.input_styled("What is the subject? \n")
         choose_subject = custom_subject
 
-    default_tv = '1' if last_name.upper()[0] <= 'M' else '2'
-
-    tv = utils.input_plus("Which TV # are you sending this to (1 for lastname A-M, 2 for N-Z, 3 for Grads)?", default_tv)
-
-    if not tv:
-        tv = default_tv
+    output_dir = "output_dir"
 
     filename = username + ".a.title"
     template = "_template.svg"
     source_file = os.path.join("panels", "TVs", template)
-    temp_filepath_svg = os.path.join(temp_dir,f"{filename}.svg")
+    temp_filepath_svg = os.path.join(output_dir, f"{filename}.svg")
     filename_png = f"{filename}.png"
-    temp_filepath_png = os.path.join(temp_dir, filename_png)
+    out_filepath_png = os.path.join(output_dir, filename_png)
 
     # creates copy of template with the filename it will use
     os.system("copy {} {}".format(source_file, temp_filepath_svg))
@@ -100,42 +89,7 @@ def add_new_title():
     inkscape_command = f'{utils.INKSCAPE} --export-filename={temp_filepath_png} -w 1920 -h 1080 {temp_filepath_svg}'
     os.system(inkscape_command)
 
-    server_filepath = "tv{}/{}/".format(tv, username)
 
-    # setup a connect so we can makesure the directory exists
-    ssh_connection = SSH(hostname, SERVER_USERNAME, pi.password)
-    # make sure the directory exists, if not create it:
-    if not ssh_connection.file_exists(server_filepath):
-        ssh_connection.send_cmd('mkdir {}'.format(server_filepath))
-
-    ## REWORK ##
-    # # move image onto the server with scp (this will fail if they've never connected to hightower before, hence warning at bottom)
-    # command = 'sshpass -p "{}" scp {} {}@{}:{}'.format( temp_filepath_png,
-    #                                                    SERVER_USERNAME, hostname, server_filepath)
-
-    # Refactor uses PuTTY (putty scpp)
-    command = f'{utils.PSCP} -pw {pi.password} {temp_filepath_png} {SERVER_USERNAME}@{hostname}:{server_filepath}'
-    print(command)
-    exit_code = os.system(command)
-    if exit_code > 0:
-        print(f"Error.  Exit code {exit_code} running command: {command}")
-
-    os.remove(temp_filepath_png)
-    os.remove(temp_filepath_svg)
-
-
-    # Check if file now exists on the server
-    title_exists = ssh_connection.file_exists(server_filepath, filename_png)
-
-    if title_exists:
-        utils.print_success(f"{filename_png} was successfully sent over to TV {tv}")
-        add_images = utils.confirm(f"Would you like to add images to {fullname}'s new shrine?")
-        if add_images:
-            add_new_media(username, tv)
-        else:
-            gen_video = utils.confirm("Would you like to regenerate the video file?")
-            if gen_video:
-                refresh_slideshow(username=username)
-    else:
+    if not os.path.isfile(filename_png):
         utils.print_error(f"The title image '{filename_png}' was not added. Maybe inkscape isn't installed? Or it's possible you've never connected to this server before. \n\n"  
                           "Try connecting once first by typing `ssh hightower` into a terminal, then answering yes.")

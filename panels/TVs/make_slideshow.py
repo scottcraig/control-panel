f1f59e6836fa18044899359059bc7835a8dd938f
scottcraig <scott.craig.victoria@gmail.com>
2024-05-08 20:23:21 -0700

    give up for today

--- panels/TVs/make_slideshow.py
+++ panels/TVs/make_slideshow.py
@@ -29,9 +29,9 @@ def make_slideshow(username=None):
 
     make_title_png(username, media_out_folder_path)
     
-    for file in os.scandir(media_folder):
+    for dir_entry in os.scandir(media_folder):
 
-        filepath = os.path.join(media_folder, file.name)
+        filepath = os.path.join(media_folder, dir_entry.name)
 
         filepath_base, ext = os.path.splitext(filepath)
 
@@ -41,24 +41,24 @@ def make_slideshow(username=None):
         except KeyError:
             # un supported extension
             expected_mime_type = None
+            print(f"{filepath} mime type not supported.\n")
+            continue
 
-        # checks if file is what it really says it is
-        mime_type_good = utils.verify_mimetype(
-            filepath, expected_mime_type)
 
         # If correct mime type verify integrity of media file
-        if mime_type_good:
+        if utils.verify_mimetype(filepath, expected_mime_type):
             success, fixed_url, ext = utils.verify_image_integrity(filepath, expected_mime_type, ext)
             if success:
                 shutil.move(fixed_url, media_out_folder_path)
             else:
                 print("Error: problem with media could not be fixed. Skipping {}\n".format(media_folder))
-                break
+                continue
         else:
             print("Unexpected file extension for {}".format(media_folder))
-            break
+            continue
 
         output_name = os.path.join(utils.OUTPUT_DIR, username + ".a.mp4")
+        print(output_name)
         movie_maker_fade.movie_maker_fade(images_directory=media_out_folder_path, output_file=output_name)
 
 

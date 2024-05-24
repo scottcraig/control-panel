177e11959985ae4f78801c0d005a02f9c39fd0f9
scottcraig <scott.craig.victoria@gmail.com>
2024-05-24 15:12:22 -0700

    Update mp4s_list_or_delete_from_TV.py

--- panels/TVs/mp4s_list_or_delete_from_TV.py
+++ panels/TVs/mp4s_list_or_delete_from_TV.py
@@ -1,5 +1,5 @@
 
-from ._mp4s_utils import choose_TV
+from ._mp4s_utils import choose_TV, choose_files
 
 def mp4s_list_or_delete_from_TV():
     print("This function delets mp4 files from a TV you specify.\n")
